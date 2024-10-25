e257529312b6ad57052818d83d38b50d8ea3ac6f
Owczarek Kamil <bilimig@wp.pl>
2024-10-25 16:06:32 +0200

    created test for redirecting url

--- app/tests.py
+++ app/tests.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine
 from .database import Base
 from .main import app, get_db
-from .models import URL
 
 
 DATABASE_URL = "postgresql+asyncpg://test_user:test_password@db/test_db"
@@ -26,19 +25,29 @@ async def db_session():
     await test_engine.dispose()
 
 @pytest.fixture(autouse=True)
-def override_get_db(db_session):
+async def override_get_db(db_session):
     async def _override_get_db():
-        yield db_session
+        async with db_session() as session:
+            yield session
 
     app.dependency_overrides[get_db] = _override_get_db
 
 @pytest.mark.asyncio
-async def tests():
+async def test_create_url():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.post("/shorten", json={"original_url": "https://example.com"})
-
+        
     assert response.status_code == 200
     assert "short_url" in response.json()
     assert response.json()["original_url"] == "https://example.com"
 
+@pytest.mark.asyncio
+async def test_redirect_to_url():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        create_response = await ac.post("/shorten", json={"original_url": "https://example.com"})
+        assert create_response.status_code == 200
+        created_url = create_response.json()["short_url"]
 
+        response = await ac.get(f"/{created_url}")
+        assert response.status_code == 200
+        assert response.json()["original_url"] == "https://example.com"
